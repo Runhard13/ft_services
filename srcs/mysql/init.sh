a67f000a167112d02ec15ea6d4299ceeb5876eb7
cdrennan <r9375806222@gmail.com>
2021-01-02 20:02:11 +0300

    ggg

--- srcs/mysql/init.sh
+++ srcs/mysql/init.sh
@@ -6,5 +6,6 @@ echo "CREATE DATABASE wordpress;" |mysql -u root --skip-password
 echo "CREATE USER 'user'@'%' IDENTIFIED BY 'password';" |mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';" |mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" |mysql -u root --skip-password
+mariadb < wordpress.sql -u root
 /etc/init.d/mariadb stop
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
